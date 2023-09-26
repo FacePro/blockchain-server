bb01e75b61d4856b6f639d185f584a21f47da779
daniel-face <daniel@facedao.pro>
2023-09-25 23:21:30 -0400

    fixed

--- lambda-functions/collectTokens/test.js
+++ lambda-functions/collectTokens/test.js
@@ -3,7 +3,7 @@ const { handler } = require('./index'); // assuming your lambda is in index.js
 async function run() {
   try {
     const result = await handler({
-      // Pass any event object you want to test with
+      // Pass any event object you want to test with new
     });
     console.log('Function executed successfully:', result);
   } catch (err) {
--- terraform/common/main.tf
+++ terraform/common/main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_iam_role" "collect_tokens_lambda_role" {
+resource "aws_iam_role" "lambda_role" {
   name = "collectTokens_lambda_role"
 
   assume_role_policy = jsonencode({
@@ -19,7 +19,7 @@ resource "aws_iam_role" "collect_tokens_lambda_role" {
 resource "aws_lambda_function" "collectTokens" {
   function_name = "collectTokens"
   handler       = "index.handler"
-  role          = aws_iam_role.collect_tokens_lambda_role.arn
+  role          = aws_iam_role.lambda_role.arn
   runtime       = "nodejs18.x"
   filename      = "collectTokens/lambda-packages/collectTokens.zip"
 
